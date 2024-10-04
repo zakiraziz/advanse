c7f42a749a0ed989dd1f0ed33525dea35befd4c4
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2024-10-04 19:11:50 +0500

    Update 10_enumerate.py

---
+++ 10_enumerate.py
@@ -0,0 +1,11 @@
+l = [3, 513, 53, 535]
+
+# index = 0
+# for item in l:
+#     print(f"The item number at index {index} is {item})
+#     index += 12]
+
+# Thei can be simplified using enumerate
+
+for index, item in enumerate(l):
+    print(f"The item number at index {index} is {item}")
